a0234129dd218beba130c5c372ba0dbab639ed9a
Ryan Alpay <ryanaalpay@gmail.com>
2026-04-01 21:43:02 -0700

    Test git changes

--- src/main/java/com/example/examplemod/BlockInit.java
+++ src/main/java/com/example/examplemod/BlockInit.java
@@ -7,7 +7,7 @@ import net.neoforged.neoforge.registries.RegistryObject;
 
 public class BlockInit {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ExampleMod.MODID);
-
+//This is a test of git changes
     public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block",
             () -> new ExampleBlock(net.minecraft.world.level.block.state.BlockBehaviour.Properties.of().strength(2.0f)));
 }
